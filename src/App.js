4f6f54ebf21d158bcff70eaeb601b7b8ebabd200
James Gary <gmarchesi888@gmail.com>
2019-07-23 09:11:23 -0400

    Display list.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,44 @@ import React, { Component } from "react";
 import Table from "./Table";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="container-primary">
-      <h2>App Component</h2>
-      <Table />
-    </div>
-  );
+class App extends Component {
+  state = {
+    birds: [
+      {
+        birdId: 100,
+        birdName: "Black-Capped Chickadee",
+        birdHabitat: "Mature Forest"
+      },
+      {
+        birdId: 101,
+        birdName: "American Goldfinch",
+        birdHabitat: "Grassland"
+      },
+      {
+        birdId: 102,
+        birdName: "Indigo Bunting",
+        birdHabitat: "Open Woodland"
+      },
+      {
+        birdId: 103,
+        birdName: "Scarlet Tanager",
+        birdHabitat: "Mature Forest"
+      },
+      {
+        birdId: 104,
+        birdName: "Blue Jay",
+        birdHabitat: "Open Woodland"
+      }
+    ]
+  };
+  render() {
+    return (
+      <div className="container-primary">
+        <h3>App Component</h3>
+        <Table birdList={this.state.birds}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Table.js
+++ src/Table.js
@@ -11,28 +11,39 @@ const TableHead = () => {
     </thead>
   );
 };
-const TableBody = () => {
-  return (
-    <tbody>
-      <tr>
-        <td>100</td>
-        <td>Scarlet Tanager</td>
-        <td>North America</td>
+
+const TableBody = props => {
+  console.log("Props:", props.birdList.map(entry => entry.birdName));
+  let birdRows = props.birdList.map(entry => {
+    return (
+      <tr key={entry.birdId}>
+        <td>{entry.birdId}</td>
+        <td>{entry.birdName}</td>
+        <td>{entry.birdHabitat}</td>
+        <td>
+          <button>Edit</button>
+        </td>
+        <td>
+          <button>Delete</button>
+        </td>
       </tr>
-    </tbody>
-  );
+    );
+  });
+  return <tbody>{birdRows}</tbody>;
 };
 
-const Table = () => {
-  return (
-    <div className="container-secondary">
-      <h3>Bird Table</h3>
-      <table>
-        <TableHead />
-        <TableBody />
-      </table>
-    </div>
-  );
-};
+class Table extends Component {
+  render() {
+    return (
+      <div className="container-secondary">
+        <h3>Bird Table</h3>
+        <table>
+          <TableHead />
+          <TableBody birdList={this.props.birdList} />
+        </table>
+      </div>
+    );
+  }
+}
 
 export default Table;
